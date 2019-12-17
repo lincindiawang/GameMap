5f4bc1c6caf97f515ed8d18659399edc448e17fc
lincindiawang <lindawang0207@gmail.com>
2019-12-17 16:27:39 +0800

    1217 1st

---
+++ app/src/main/java/com/linda/game/MainActivity.java
@@ -0,0 +1,52 @@
+package com.linda.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Button bLeft = findViewById(R.id.button_left);
+//        bLeft.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+        findViews();
+    }
+
+    private void findViews() {
+        findViewById(R.id.arrow_down).setOnClickListener(this);
+        findViewById(R.id.arrow_up).setOnClickListener(this);
+        findViewById(R.id.arrow_right).setOnClickListener(this);
+        findViewById(R.id.arrow_left).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.arrow_left:
+                Log.d("MainActivity","onClick : LEFT");
+                break;
+            case R.id.arrow_right:
+                Log.d("MainActivity","onClick : RIGHT");
+                break;
+            case R.id.arrow_up:
+                Log.d("MainActivity","onClick : UP");
+                break;
+            case R.id.arrow_down:
+                Log.d("MainActivity","onClick : DOWN");
+                break;
+        }
+
+    }
+}
